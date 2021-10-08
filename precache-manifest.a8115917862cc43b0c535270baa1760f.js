f2c5554ba422ce75c4cd0fb84e9d10cc1b8ce074
Kate Kuzmenko <rina.punina@gmail.com>
2021-10-08 14:56:49 +0300

    Updates

---
+++ precache-manifest.a8115917862cc43b0c535270baa1760f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d08cdaf9b15293465014e74c05fcb98",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1e3fdfa1a5c632ee2371",
+    "url": "./static/css/main.ae4f840b.chunk.css"
+  },
+  {
+    "revision": "bef14ca31350064182c6",
+    "url": "./static/js/2.e8d9cca5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e8d9cca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e3fdfa1a5c632ee2371",
+    "url": "./static/js/main.f8c18d10.chunk.js"
+  },
+  {
+    "revision": "e447b2ddaa3314191b11",
+    "url": "./static/js/runtime-main.e90a3edd.js"
+  }
+]);
